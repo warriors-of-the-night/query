574a15aacc3c75afd42305ba7e34488b5bc31792
范捷琦 <jieqi.fan@corp.elong.com>
2014-12-18 13:47:25 +0800

    change the xpath of seo_ranks_host of baidu. try to extract the host of image.baidu.com

--- lib/query/result/baidu.rb
+++ lib/query/result/baidu.rb
@@ -65,7 +65,7 @@ module Query
 
       def parse_seo(div)
         title = %w(div[1]/h3/a h3/a).inject(nil){|ans, xpath| ans || div.xpath(xpath).first}
-        url   = %w(span[@class="g"] span[@class="c-showurl"] span[@class="op_wiseapp_showurl"] div[@class="op_zhidao_showurl"]).inject(nil){|ans, xpath| ans || div.search(xpath).first}
+        url   = %w(span[@class="g"] span[@class="c-showurl"]/span[@class="c-showurl"] span[@class="c-showurl"] span[@class="op_wiseapp_showurl"] div[@class="op_zhidao_showurl"]).inject(nil){|ans, xpath| ans || div.search(xpath).first}
         url = url.nil? ? 'www.baidu.com' : url.text.sub(/\d{4}-\d{1,2}-\d{1,2}/,'').strip
         url = "http://" + url
 
