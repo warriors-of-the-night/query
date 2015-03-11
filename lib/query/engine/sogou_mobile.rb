ef048c0b4f72f22d31cea3f3dccdddf761211dcc
timlentse <tinglenxan@gmail.com>
2015-03-11 11:21:22 +0800

    add params to query of sogou_mobile

--- lib/query/engine/sogou_mobile.rb
+++ lib/query/engine/sogou_mobile.rb
@@ -2,16 +2,22 @@ module Query
 	module Engine
 		class SogouMobile
 			include Query::Engine
-			Host = 'http://wap.sogou.com'
+			Host = 'wap.sogou.com'
 			Options = {
 				:headers => {"User-Agent" => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5'}
 			}
-			def self.query(wd)
-				path = "/web/searchList.jsp?keyword=#{wd}"
-				uri = URI.encode(Host + path)
+			def self.query(wd, params={})
+			  q = Array.new
+        q << "keyword=#{URI.encode(wd)}"
+        #q << "rn=#{@perpage.to_i}" if @perpage
+        # Join arguments
+        params.each do |k, v|
+          q << "#{k.to_s}=#{v.to_s}"
+        end
+        uri = URI::HTTP.build(:host=>Host,:path=>'/web/searchList.jsp',:query=>q.join('&'))
 				res = HTTParty.get(uri, Options)
 				r = Query::Result::SogouMobile.new(res)
-				r.baseuri, r.options = URI(uri), Options  
+				r.baseuri, r.options = uri, Options  
 				r
 			end
 		end
