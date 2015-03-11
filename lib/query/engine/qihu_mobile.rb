35ed39b9e936ae469458f40b6e5ad5f7f4ed0465
timlentse <tinglenxan@gmail.com>
2015-03-11 15:30:40 +0800

    qihu featurn but not finished

--- lib/query/engine/qihu_mobile.rb
+++ lib/query/engine/qihu_mobile.rb
@@ -1,2 +1,20 @@
 module Query
+	module Engine
+		class QihuMobile
+			Host = "m.haosou.com"
+      Options = {
+        :headers => {"User-Agent" => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.73.11 (KHTML, like Gecko) Version/7.0.1 Safari/537.73.11'}
+        }
+      def self.query(wd)
+        uri = URI.join("http://#{Host}/", URI.encode('s?q='+wd))
+        page = HTTParty.get(uri, Options)
+        #如果请求地址被跳转,重新获取当前页的URI,可避免翻页错误
+        uri = URI.join("http://#{Host}/", page.request.path)
+        r = Query::Result::Qihu.new(page)
+        r.baseuri = uri
+        r.options = Options
+        r
+      end
+    end
+  end
 end
--- lib/query/result/qihu_mobile.rb
+++ lib/query/result/qihu_mobile.rb
@@ -1,6 +1,18 @@
 module Query
-    module Result
-        class QihuMobile
+  module Result
+    class QihuMobile
+      include Query::Result
+      def seo_ranks
+      	@page.search("//div[@id='main']/div/div/div/a").map.with_index do |item,index|
+          title = item.search('h3').first || item
+          {
+            :rank => index + 1,
+            :href => item['href'],
+            :text => title.text.gsub(/　|\n|\s/,""),
+            :host => Addressable::URI.parse(item['href']).host
+          }
         end
+      end
     end
+  end
 end
