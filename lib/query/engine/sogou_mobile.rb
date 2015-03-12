be0041d0386d36b371a5070735a3d9da774ad7ab
范捷琦 <jieqi.fan@corp.elong.com>
2015-03-12 15:32:57 +0800

    change the indentation.

--- lib/query/engine/sogou_mobile.rb
+++ lib/query/engine/sogou_mobile.rb
@@ -1,13 +1,13 @@
 module Query
-	module Engine
-		class SogouMobile
-			include Query::Engine
-			Host = 'wap.sogou.com'
-			Options = {
-				:headers => {"User-Agent" => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5'}
-			}
-			def self.query(wd, params={})
-			  q = Array.new
+  module Engine
+    class SogouMobile
+      include Query::Engine
+      Host = 'wap.sogou.com'
+      Options = {
+        :headers => {"User-Agent" => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_2 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8H7 Safari/6533.18.5'}
+      }
+      def self.query(wd, params={})
+        q = Array.new
         q << "keyword=#{URI.encode(wd)}"
         #q << "rn=#{@perpage.to_i}" if @perpage
         # Join arguments
@@ -15,11 +15,11 @@ module Query
           q << "#{k.to_s}=#{v.to_s}"
         end
         uri = URI::HTTP.build(:host=>Host,:path=>'/web/searchList.jsp',:query=>q.join('&'))
-				res = HTTParty.get(uri, Options)
-				r = Query::Result::SogouMobile.new(res)
-				r.baseuri, r.options = uri, Options  
-				r
-			end
-		end
-	end
+        res = HTTParty.get(uri, Options)
+        r = Query::Result::SogouMobile.new(res)
+        r.baseuri, r.options = uri, Options  
+        r
+      end
+    end
+  end
 end
--- lib/query/result/sogou_mobile.rb
+++ lib/query/result/sogou_mobile.rb
@@ -1,34 +1,34 @@
 require 'cgi'
 module Query
-	module Result
-		class SogouMobile
-			include Query::Result
-			def ads_top
-				@page.search("//div[@class='results']/div[@class='ec_ad_results'][1]/div[@class='ad_result']").map.with_index do |ad_div,index|
-					parse_ad(ad_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
-				end
-			end
+  module Result
+    class SogouMobile
+      include Query::Result
+      def ads_top
+        @page.search("//div[@class='results']/div[@class='ec_ad_results'][1]/div[@class='ad_result']").map.with_index do |ad_div,index|
+          parse_ad(ad_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
+        end
+      end
 
-			def ads_right
-				[]
-			end
+      def ads_right
+        []
+      end
 
-			def ads_bottom
-				@page.search("//div[@class='results']/div[@class='ec_ad_results'][2]/div[@class='ad_result']").map.with_index do |ad_div,index|
-					parse_ad(ad_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
-				end
-			end
+      def ads_bottom
+        @page.search("//div[@class='results']/div[@class='ec_ad_results'][2]/div[@class='ad_result']").map.with_index do |ad_div,index|
+          parse_ad(ad_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
+        end
+      end
 
-			def seo_ranks
-				@seo_rank ||= @page.search("//div[@class='results']/div[@class='result' or @class='vrResult']").map.with_index do |seo_div,index|
-				  parse_seo_ranks(seo_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
-				end
-			end
+      def seo_ranks
+        @seo_rank ||= @page.search("//div[@class='results']/div[@class='result' or @class='vrResult']").map.with_index do |seo_div,index|
+          parse_seo_ranks(seo_div).merge({:rank => (@pagenumber-1)*10 + index + 1})
+        end
+      end
 
-			def next_url
-				"#{@baseuri.to_s}&p=#{@pagenumber+1}"
-			end
-			
+      def next_url
+        "#{@baseuri.to_s}&p=#{@pagenumber+1}"
+      end
+      
       def related_keywords
         @related_keywords ||= @page.search("div[@class='hint']/ul/li/a").map{|relwd| relwd.text.gsub(/ |\n|\t/,"")}
       end
@@ -37,46 +37,46 @@ module Query
       end
       
       def html
-      	@page.to_html
+        @page.to_html
       end
 
       private
       def parse_ad(ad_div)
-      	site = ad_div.search(".//span[@class='exp_tip']/preceding-sibling::span")[0] || ad_div.search(".//div[@class='bd_citeurl']/text()")[0]
-				{
-					:text => ad_div.search('h3')[0].text.gsub(/ |\n|\t/,""),
-					:href => ad_div.search('a')[0]['href'],
-					:host => site.text.strip.downcase
-				}
+        site = ad_div.search(".//span[@class='exp_tip']/preceding-sibling::span")[0] || ad_div.search(".//div[@class='bd_citeurl']/text()")[0]
+        {
+          :text => ad_div.search('h3')[0].text.gsub(/ |\n|\t/,""),
+          :href => ad_div.search('a')[0]['href'],
+          :host => site.text.strip.downcase
+        }
       end
       
       def parse_seo_ranks(seo_div)
-      	begin
-      	a    = seo_div.search(".//a[contains(@href,'url=')]")[0]
-			  href = URI.decode(CGI.parse(URI(URI.encode(a['href'])).query)['url'][0])
-			  if href==""
-			  	href = seo_div.search(".//div[@class='citeurl']/text()")[0].text.gsub(/ |-/,"")
-			  	href = "http://#{href}"
-			  end
-			  if seo_div['class']=='result'
-			  	is_vr, title = false, a.search("./text()|./em|./span")
-			  else
-			    seo_div.css('script').remove    # remove all script tags
-			  	title = seo_div.search('.//h3')[0] || a.search("./text()|./span|./em")
-			    is_vr = true
-			  end
-					{
-						:text  => title.text.gsub(/ |\n|\t/,""),
-						:href  => href,
-						:host  => URI(URI.encode(href)).host,
-						:is_vr => is_vr
-					}
-				rescue Exception => e
+        begin
+        a    = seo_div.search(".//a[contains(@href,'url=')]")[0]
+        href = URI.decode(CGI.parse(URI(URI.encode(a['href'])).query)['url'][0])
+        if href==""
+          href = seo_div.search(".//div[@class='citeurl']/text()")[0].text.gsub(/ |-/,"")
+          href = "http://#{href}"
+        end
+        if seo_div['class']=='result'
+          is_vr, title = false, a.search("./text()|./em|./span")
+        else
+          seo_div.css('script').remove    # remove all script tags
+          title = seo_div.search('.//h3')[0] || a.search("./text()|./span|./em")
+          is_vr = true
+        end
+          {
+            :text  => title.text.gsub(/ |\n|\t/,""),
+            :href  => href,
+            :host  => URI(URI.encode(href)).host,
+            :is_vr => is_vr
+          }
+        rescue Exception => e
           warn "Error in parse_seo method : " + e.message
           {}
         end
       end
       
-		end
-	end
+    end
+  end
 end
