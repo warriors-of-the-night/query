487a75fdf14699030e55e92fddd2990e8ca17f08
timlentse <tinglenxan@gmail.com>
2015-03-13 15:03:08 +0800

    Add handler for div without 'data-log' attr

--- lib/query/result/baidu_mobile.rb
+++ lib/query/result/baidu_mobile.rb
@@ -5,22 +5,22 @@ module Query
 
       def seo_ranks
         s_res =  @page.at("//div[@id='results']")
-        @seo_ranks ||= s_res.css("div.result").map.with_index do |div,index|
-          parse_seo(div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
+        @seo_ranks ||= s_res.css("div.result").map.with_index do |seo_div,index|
+          parse_seo(seo_div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
         end
       end
 
       def ads_top
         selector = "//*[@class='result']/preceding-sibling::div[not (contains(@class,'result'))]/div/div/a[not (contains(@href,'http://baozhang.baidu.com/guarantee'))]/.."
-        @ads_top ||= @page.search(selector).map.with_index do |div,index|
-          parse_ad(div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
+        @ads_top ||= @page.search(selector).map.with_index do |ad_div,index|
+          parse_ad(ad_div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
         end
       end
 
       def ads_bottom
         selector = "//*[@class='result']/following-sibling::div[not (contains(@class,'result'))]/div/div/a[not (contains(@href,'http://baozhang.baidu.com/guarantee'))]/.."
-        @ads_bottom ||= @page.search(selector).map.with_index do |div,index|
-          parse_ad(div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
+        @ads_bottom ||= @page.search(selector).map.with_index do |ad_div,index|
+          parse_ad(ad_div).merge({:rank => (index + 1) + (@pagenumber -1) * 10})
         end
       end
 
@@ -48,16 +48,16 @@ module Query
       end
 
     private 
-      def parse_ad(div)
+      def parse_ad(ad_div)
         begin
-          title_link = div.search('a').first
-          url = div.search('link')
+          title_link = ad_div.search('a')[0]
+          url = ad_div.search('link')
           if url.empty?
-            url = div.search(".//span[contains(text(),'.com')]").first
+            url = ad_div.search(".//span[contains(text(),'.com')]")[0]
             url = url.nil? ? "http://m.baidu.com" : "http://#{url.text.strip}"
             title = title_link.text
           else
-            url = url.first['href']
+            url = url[0]['href']
             title = title_link.xpath("./text() | ./em").text
           end
            {
@@ -71,27 +71,26 @@ module Query
         end
       end
 
-      def parse_seo(div)
+      def parse_seo(seo_div)
         begin
-          title_link = div.search('a').first
+          title_link = seo_div.search('a')[0]
           href = title_link['href']
-          href = href.include?('m.baidu.com') ? href : "http://m.baidu.com#{href}"
-          if div['srcid']=='map'
-            host, is_vr = 'map.baidu.com', true
-          elsif div['class']=='result'
-            host = div.search(".//*[@class='site']").first
-            host = div.search(".//*[name()!='style' and contains(text(),'.com')]").first  if host.nil?
-            host = host.nil? ? 'm.baidu.com' : host.text.split[0]
-            is_vr = false
-          elsif div['tpl'] and div['data-log']
-            url = JSON.parse(div['data-log'].gsub("'",'"'))['mu']
+          href = href[/m.baidu.com/] ? href : "http://m.baidu.com#{href}"
+          if seo_div['class']=='result'
+            host, is_vr = seo_div.search(".//*[@class='site']")[0], false
+            host = host.nil? ? find_host(seo_div) : host.text.split[0] 
+          elsif seo_div['srcid']=='map'
+            is_vr, host = true, 'map.baidu.com'          
+          elsif seo_div['tpl'] and seo_div['data-log']
+            url = JSON.parse(seo_div['data-log'].gsub("'",'"'))['mu']
             if url==''
-            	host = div.search(".//*[name()!='style' and contains(text(),'.com')]").first
-              host = host.nil? ? 'm.baidu.com' : host.text.split[0]
+            	host = find_host(seo_div)
             else
               host = Addressable::URI.parse(URI.encode(url)).host
             end
             is_vr = true
+          else 
+          	is_vr, host = true, find_host(seo_div)
           end
           #is_vr = (is_vr.nil? and !host[/baidu|nuomi/]) ? false : true     
           {   
@@ -105,7 +104,12 @@ module Query
           {}
         end
       end
-
+      
+			def find_host(node)
+				host = node.search(".//*[name()!='style' and (contains(text(),'.cn') or contains(text(),'com'))]")[0]
+				host.nil? ? 'm.baidu.com' : host.text.split[0]
+			end
+			
       def redirect(url,limit = 10) 
         raise ArgumentError, 'Too many HTTP redirects' if limit == 0
         response = Net::HTTP.get_response(URI(url))
