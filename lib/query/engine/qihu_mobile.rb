e22f2b8d4108cae2a47e84a6cdcfcb06df7bef46
timlentse <tinglenxan@gmail.com>
2015-03-16 12:24:35 +0800

    qihu finished

--- lib/query/engine/qihu_mobile.rb
+++ lib/query/engine/qihu_mobile.rb
@@ -5,14 +5,18 @@ module Query
       Options = {
         :headers => {"User-Agent" => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.73.11 (KHTML, like Gecko) Version/7.0.1 Safari/537.73.11'}
         }
-      def self.query(wd)
-        uri = URI.join("http://#{Host}/", URI.encode('s?q='+wd))
-        page = HTTParty.get(uri, Options)
-        #如果请求地址被跳转,重新获取当前页的URI,可避免翻页错误
-        uri = URI.join("http://#{Host}/", page.request.path)
-        r = Query::Result::Qihu.new(page)
-        r.baseuri = uri
-        r.options = Options
+      def self.query(wd, params={})
+        q = Array.new
+        q << "q=#{URI.encode(wd)}"
+        #q << "rn=#{@perpage.to_i}" if @perpage
+        # Join arguments
+        params.each do |k, v|
+          q << "#{k.to_s}=#{v.to_s}"
+        end
+        uri = URI::HTTP.build(:host=>Host,:path=>'/s',:query=>q.join('&'))
+        res = HTTParty.get(uri, Options)
+        r = Query::Result::QihuMobile.new(res)
+        r.baseuri, r.options = uri, Options  
         r
       end
     end
--- lib/query/result/qihu_mobile.rb
+++ lib/query/result/qihu_mobile.rb
@@ -3,16 +3,29 @@ module Query
     class QihuMobile
       include Query::Result
       def seo_ranks
-      	@page.search("//div[@id='main']/div/div/div/a").map.with_index do |item,index|
-          title = item.search('h3').first || item
-          {
-            :rank => index + 1,
-            :href => item['href'],
-            :text => title.text.gsub(/　|\n|\s/,""),
-            :host => Addressable::URI.parse(item['href']).host
+      	@page.css('div.g-card').map.with_index do |seo_div,index|
+      		a = seo_div.at('.//a[contains(@href,"u=")]')
+          title = seo_div.at_css('h3') || a
+          cite = seo_div.at('.//*[@class="res-show-url"]/text()')
+          cite = URI.decode(CGI.parse(URI(URI.encode(a['href'])).query)['u'][0]) if !cite
+          cite = "http://" + cite.to_s if !cite.to_s[/http/]
+          {	:is_vr => seo_div['class']=="g-card r-og-card" ? true : false,
+            :rank  => index + 1,
+            :href  => a['href'],
+            :text  => title.text.gsub(/　|\n|\s/,""),
+            :host  => URI(URI.encode(cite.gsub(/ |-/,''))).host
           }
         end
       end
+      
+      def next_url
+       "#{@baseuri.to_s}&pn=#{@pagenumber+1}"
+      end
+      
+      def html
+        @page.to_html
+      end
+
     end
   end
 end
